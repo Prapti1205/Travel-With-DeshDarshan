5a1c2462987722f27b246d20942aa505e0a3823e
Prapti sharma <121335869+Prapti1205@users.noreply.github.com>
2024-08-12 14:50:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+
+const sliderImgs = ["image1.png", "image2.png", "image3.png", "image4.png"];
+let sliderImage = document.querySelector('.background-image');
+let sliderGrids = [...document.querySelectorAll('.grid-item')];
+
+let currentImage = 0;
+
+setInterval(() => {
+    changeSliderImage();
+}, 5000);
+
+const changeSliderImage = () => {
+    sliderGrids.map((gridItem, index) => {
+        setTimeout(() => {
+            gridItem.classList.remove('hide');
+
+            setTimeout(() => {
+
+                if(index == sliderGrids.length - 1){
+                    if(currentImage >= sliderImgs.length - 1){
+                        currentImage = 0;
+                    } else{
+                        currentImage++;
+                    }
+
+                    sliderImage.src = `img/${sliderImgs[currentImage]}`;
+
+                    sliderGrids.map((item, i) => {
+                        setTimeout(() => {
+                            item.classList.add('hide')
+                        }, i * 100);
+                    })
+
+                }
+
+            }, 100);
+
+        }, index * 100);
+    })
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if(scrollY >= 188){
+        navbar.classList.add('bg');
+    } else{
+        navbar.classList.remove('bg')
+    }
+})
